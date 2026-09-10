c64018d48ab19b4a46cf46d7da7c179a76b2ba16
siddhartha-yz <64243438+siddhartha-yz@users.noreply.github.com>
2026-09-10 20:41:57 +0800

    26.9.10 学习笔记更新

---
+++ PyTorch语法/py/5.py
@@ -0,0 +1,35 @@
+import torch
+
+x_data = torch.tensor([[1.0],[2.0],[3.0]])
+y_data = torch.tensor([[2.0],[4.0],[6.0]])
+
+class LinearModel(torch.nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.linear = torch.nn.Linear(1,1)
+
+    def forward(self, x):
+        y_pred = self.linear(x)
+        return y_pred
+
+model = LinearModel()
+criterion = torch.nn.MSELoss(reduction='mean')
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+for epoch in range(100):
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+print('w=',model.linear.weight.item())
+print('b=',model.linear.bias.item())
+
+x_test = torch.tensor([[4.0]])
+y_test = model(x_test)
+print('y_pred=',y_test.detach())
+
+
